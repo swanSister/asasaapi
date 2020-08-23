3105abd1bf9ba6e005e14071fa2793cc638db487
yjjung@mac <yjjung@tecace.com>
2020-08-23 14:12:52 +0900

    testcod api

--- query.js
+++ query.js
@@ -1,21 +1,25 @@
 
 var dbconfig   = require('./config/database.js');
 //var mysql      = require('mysql');
-const mysql = require(`mysql-await`);
-const connection = mysql.createConnection(dbconfig);
+const mysql = require(`mysql-await`)
+const connection = mysql.createConnection(dbconfig)
  
 connection.on(`error`, (err) => {
-  console.error(`Connection error ${err.code}`);
+  console.error(`Connection error ${err.code}`)
 });
  
 
 module.exports = async function (sql) {
   try{
     let res = await connection.awaitQuery(sql)
+    connection.destroy()
+    connection = mysql.createConnection(dbconfig)
     return {success:true, data:res}
   }catch(e){
-			console.log('Query Error', e.message);
-			return {success:false, errorMessage:e.message};
+      console.log('Query Error', e)
+      connection.destroy()
+      connection = mysql.createConnection(dbconfig)
+			return {success:false, errorMessage:e}
   }
   
 };
